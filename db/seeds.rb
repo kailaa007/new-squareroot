2c9f25e2c53bbe3e223b4c4c505f08b065f97904
Amit <amitkumar@mobikasa.com>
2017-01-02 11:43:19 +0530

    fix db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
 
-#Administrator.create(name: "admin123", email: "admin1@mobikasa.com", password: "mobikasa33", super_admin: true)
-#puts "admin created"
+Administrator.create(name: "mobikasa", email: "admin123@mobikasa.com", password: "12345678", super_admin: true)
+puts "admin created"
 BirthPlan.create(title: "BirthPlan 1")
 puts "BirthPlan created"
