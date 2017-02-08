98d80b7f87b7c1d3d7cb27c8d540fefbee222f05
ashimwilliam <ashimwilliam@gmail.com>
2017-02-08 12:31:05 +0530

    add js

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -61,9 +61,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # The path used after sign up for inactive accounts.
-  def after_inactive_sign_up_path_for(resource)
-    puts "---------------------------------------------------------------"
-        birth_plans_path
+  def after_inactive_sign_up_path_for(resource) 
+    birth_plans_path
   end
 
 end
