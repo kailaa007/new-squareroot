6fd0ca24e24612b4567e2b809af3eaab07734f22
Nitesh <nitesh@cogitohub.com>
2017-03-03 18:52:37 +0530

    fix

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -16,19 +16,7 @@ class Admin::UsersController < Admin::ApplicationController
 
   def birth_plan_report
     @user = User.find(params[:id])
-    @answers = BirthPlanAnswer.where("user_id = ?", @user.id)
-    
-    @questions = []
-    questions = @answers.pluck(:question_id).uniq 
-
-    questions.each do |ques|
-      begin
-        @questions <<  Question.find(ques)
-      rescue Exception => e
-        logger.error e.message    
-        next
-      end
-    end   
+    @birth_plan_answers = @user.birth_plan_answers
   end  
   
 end
