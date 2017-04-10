a1e8d84e4faeeb52b8a53bc7c7393e3483c45a28
Nitesh <nitesh@cogitohub.com>
2017-04-11 01:04:45 +0530

    admin fix

--- app/controllers/birth_plan_answers_controller.rb
+++ app/controllers/birth_plan_answers_controller.rb
@@ -1,6 +1,6 @@
 class BirthPlanAnswersController < ApplicationController
   layout 'devise'
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, except: [:save_session]
   
   def get_restriction
     @restriction = []
