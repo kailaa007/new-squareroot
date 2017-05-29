7a142a2210c7c85e8c1ed87bd70a908ff85a5f90
ashimwilliam <ashimwilliam@gmail.com>
2017-05-30 05:14:03 +0530

    mailer setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,15 +38,15 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'sroottest.herokuapp.com', port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
+    :address => 'smtp.sendgrid.net',
     :port => "587",
     :domain => "mobikasa.com",
-    :user_name => "anoop@mobikasa.com",
-    :password => "mobikasa@",
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :authentication => "plain",
     :enable_starttls_auto => true
   } 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,15 +77,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'http://squareroot.herokuapp.com/'}
+  config.action_mailer.default_url_options = { host: 'sroottest.herokuapp.com'}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
+   :address => 'smtp.sendgrid.net',
     :port => "587",
     :domain => "mobikasa.com",
-    :user_name => "anoop@mobikasa.com",
-    :password => "mobikasa@",
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :authentication => "plain",
     :enable_starttls_auto => true
   } 
